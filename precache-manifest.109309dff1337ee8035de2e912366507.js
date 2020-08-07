dd506ff2767c4a48dde1a9e95d5382fe070863ab
Manishankar04 <es.maniobu@gmail.com>
2020-08-07 10:00:45 +0530

    Updates

---
+++ precache-manifest.109309dff1337ee8035de2e912366507.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447ce7a4d90d85b80798cbe4a109d508",
+    "url": "/Manishankar04/friends/index.html"
+  },
+  {
+    "revision": "4016fac7fce10e065bce",
+    "url": "/Manishankar04/friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1b8d46ace517c6879481",
+    "url": "/Manishankar04/friends/static/css/main.7d766b71.chunk.css"
+  },
+  {
+    "revision": "4016fac7fce10e065bce",
+    "url": "/Manishankar04/friends/static/js/2.a6e8b5b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Manishankar04/friends/static/js/2.a6e8b5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8d46ace517c6879481",
+    "url": "/Manishankar04/friends/static/js/main.7a935e13.chunk.js"
+  },
+  {
+    "revision": "656e70292b831dc59379",
+    "url": "/Manishankar04/friends/static/js/runtime-main.bc889905.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Manishankar04/friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
